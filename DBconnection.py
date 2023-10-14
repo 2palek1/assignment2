c46e6b0086d395d4a31aca9b14c7a01127b220dd
Askhat <ashimov.ashat05@gmail.com>
2023-10-14 17:33:38 +0600

    working version

--- DBconnection.py
+++ DBconnection.py
@@ -1,12 +1,15 @@
 import psycopg2 as pg
+import json
 
+
+# Connection to DB
 try:
     conn = pg.connect(
         host="localhost",
         database="ass2",
         port=5432,
-        user="postgres",
-        password="1337"
+        user="username",
+        password="password"
     )
     cursor = conn.cursor()
     print("Connection established")
@@ -18,37 +21,95 @@ except Exception as err:
     print(err)
 
 
+# Returns tables list
 def get_tables(curs):
     curs.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
     return curs.fetchone()
 
 
+# Returns table schema
 def get_schema(table, curs):
-    curs.execute(f"SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_name = 'public.{table}'")
+    curs.execute(f"SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_name = '{table}';")
     return curs.fetchall()
 
 
+# Prints table content
 def display_content(table, curs):
-    curs.execute(f"SELECT * FROM {table}")
+    curs.execute(f"SELECT * FROM {table};")
     contents = curs.fetchall()
-    for c in contents:
-        print(c)
+    for cont in contents:
+        print(cont)
 
 
-def create_row(table, curs):
-    pass
+# Creates new row
+def create_row(table, curs, con):
+    title = input("Enter title: ")
+    price = input("Enter price: ")
+    address = input("Enter address: ")
+    link_id = input("Enter the advertisement ID: ")
+    link = f"https://krisha.kz/a/show/{link_id}"
+
+    try:
+        curs.execute(f"INSERT INTO offers (title, price, address, link_id, link) VALUES ('{title}', '{price}', '{address}', '{link_id}', '{link}');")
+        con.commit()
+        print("Row inserted successfully.")
+
+    except Exception as err:
+        con.rollback()  # Rollback changes in case of an error
+        print("Error inserting row:")
+        print(err)
 
 
-def change_row(table, curs):
-    pass
+# Changes existing row by ID
+def change_row(table, curs, con):
+    id = int(input("Which row do you want to change? "))
+    title = input("Enter title: ")
+    price = input("Enter price: ")
+    address = input("Enter address: ")
+    link_id = input("Enter the advertisement ID: ")
+    link = f"https://krisha.kz/a/show/{link_id}"
+
+    try:
+        curs.execute(f"UPDATE offers SET title = '{title}', price = '{price}', address = '{address}', link_id = '{link_id}', link = '{link}' WHERE id = {id};")
+        con.commit()
+        print("Row updated successfully.")
+
+    except Exception as err:
+        con.rollback()  # Rollback changes in case of an error
+        print("Error updating row:")
+        print(err)
 
 
-def delete_row(table, curs):
-    pass
+# Deletes existing row by id
+def delete_row(table, curs, con):
+    id = int(input("Which row do you want to delete? "))
+
+    try:
+        curs.execute(f"DELETE FROM offers WHERE id = {id};")
+        con.commit()
+        print("Row deleted successfully.")
+
+    except Exception as err:
+        con.rollback()  # Rollback changes in case of an error
+        print("Error deleting row:")
+        print(err)
 
 
-def load_json(table, json_file, curs):
-    pass
+# inserting data from json to table
+def load_json(table, json_file, curs, con):
+    with open(json_file, 'r') as data:
+        extracted_data = json.load(data)
+        for item in extracted_data:
+            try:
+                curs.execute(f"""
+                INSERT INTO offers (title, price, address, link_id, link)
+                VALUES ('{item["title"]}', '{item["price"]}', '{item["address"]}', '{item["id"]}', '{item["link"]}');
+                """)
+                con.commit()
+                print("Row inserted successfully.")
 
-
-display_content('offers', cursor)
+            except Exception as err:
+                con.rollback()  # Rollback changes in case of an error
+                print("Error inserting JSON:")
+                print(err)
+                break
